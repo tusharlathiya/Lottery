a6fda77b68fd64c9bcc845ed0b77a6878aab9e8b
Tushar Lathiya <tusharl@thoughtworks.com>
2018-10-17 22:09:16 +0530

    Added Lottry Contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.4.24;
+
+/**
+ *  This contract is used as lottery scheme, master get 20% and winner get 80%
+ */
+contract Lottery {
+
+    // Status of Scheme
+    enum Status { ACTIVE, EXPIRED }
+
+    uint schemeId = 1;
+
+    mapping (uint => Scheme) schemes;
+
+    struct Scheme {
+        address                  master;
+        address                  winner;
+        Status                   status;
+        uint                     amount;
+        uint                     maxAmount;
+        uint                     totalTokenSold;
+        mapping (uint => Player) players;
+    }
+
+    struct Player {
+        address playerAddress;
+        uint    amount;
+    }
+
+    event playerAdded(string,address,uint);
+    event winnerDeclared(string,address);
+    event logInteger(string,uint);
+
+    modifier masterOnlyAccess(uint _schemeId) {
+        require(schemes[_schemeId].master == msg.sender);
+        _;
+    }
+
+    function generateScheme(uint maxAmount) public returns (uint _schemeId) {
+        _schemeId          = schemeId++;
+        schemes[_schemeId] = Scheme(msg.sender, 0x0, Status.ACTIVE, 0, maxAmount, 0);
+        emit logInteger('Scheme generated', _schemeId);
+    }
+
+    function buyToken(uint _schemeId) public payable {
+        require(schemes[_schemeId].status == Status.ACTIVE, 'Scheme must be active.');
+        require(msg.value == 1 wei, 'Token Amount must be 1.');
+        require(schemes[_schemeId].amount <= schemes[_schemeId].maxAmount, 'Scheme capacity reached.');
+
+        Scheme storage scheme = schemes[_schemeId];
+        scheme.players[scheme.totalTokenSold++] = Player(msg.sender, msg.value);
+        scheme.amount += msg.value;
+
+        emit playerAdded('player added', msg.sender, msg.value);
+    }
+
+    function declareWinner(uint _schemeId) public payable masterOnlyAccess(_schemeId) {
+        require(schemes[_schemeId].status == Status.ACTIVE, 'Scheme must be active.');
+
+        Scheme storage scheme = schemes[_schemeId];
+        scheme.status = Status.EXPIRED;
+        scheme.winner = scheme.players[generateRandomWithin(scheme.totalTokenSold)].playerAddress;
+
+        uint winningAmount = (scheme.amount * 80) / 100;
+
+        emit winnerDeclared('winner', scheme.winner);
+        emit logInteger('Winning Amount', winningAmount);
+
+        scheme.winner.transfer(winningAmount);
+        scheme.master.transfer(scheme.amount-winningAmount);
+    }
+
+    function generateRandomWithin(uint limit) private view returns (uint) {
+        uint Q = block.difficulty / block.number;
+        uint R = block.difficulty % block.number;
+
+        uint random = ( block.difficulty * (now % Q)) - ( R * (now / Q));
+
+        if(random <= 0) {
+            random = random + block.difficulty;
+        }
+         return random % limit;
+    }
+}
